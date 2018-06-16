ba1c02cfd45651c9ee973144141d4345b9a85263
Mohit Balu <mohitbalu87.mb@gmail.com>
2018-06-16 01:37:27 -0700

    Update and rename keydetect.py to micKeyDetector.py

--- micKeyDetector.py
+++ micKeyDetector.py
@@ -5,7 +5,7 @@ import os, signal
 from sys import stdout
 from re import split
 
-class Proc(object):
+class Process(object):
     ''' Data structure to store the output of 'ps aux' command '''
     def __init__(self, proc_info):
         self.user = proc_info[0]
@@ -21,19 +21,20 @@ class Proc(object):
         self.cmd = proc_info[10]
 
     def to_str(self):
-        ''' Returns a string containing minimalistic info
-        about the process : user, pid, and command '''
+        ''' Return user, pid, and command '''
         return '%s %s %s' % (self.user, self.pid, self.cmd)
+    
     def name(self):
 	''' Return command only'''
 	return '%s' %self.cmd	
+   
     def procid(self):
 	'''Return pid only'''
 	return '%s' %self.pid
 
 
 def kill_logger(key_pid):
-    print("Do you want to stop this keylogger: y/n ?")
+    stdout.write("\n\nDo you want to stop this process: y/n ?"),
     response = raw_input()
     if (response=="y" or response =="Y"):
 	os.kill(int(key_pid), signal.SIGKILL)
@@ -41,47 +42,45 @@ def kill_logger(key_pid):
 	pass
 	 
 
-def get_proc_list():
-    ''' Retrieves a list [] of Proc objects representing the active
-    process list list '''
-    proc_list = []
-    sub_proc = Popen(['ps', 'aux'], shell=False, stdout=PIPE)
+def get_process_list():
+    ''' Retrieves a list of Process objects representing the active process list list '''
+    process_list = []
+    sub_process = Popen(['ps', 'aux'], shell=False, stdout=PIPE)
     #Discard the first line (ps aux header)
-    sub_proc.stdout.readline()
-    for line in sub_proc.stdout:
+    sub_process.stdout.readline()
+    for line in sub_process.stdout:
         #The separator for splitting is 'variable number of spaces'
         proc_info = split(" *", line.strip())
-        proc_list.append(Proc(proc_info))
-    return proc_list
+        process_list.append(Process(proc_info))
+    return process_list
 
 if __name__ == "__main__":
-	proc_list = get_proc_list()
-	#Show the minimal proc list (user, pid, cmd)
-	stdout.write('Process list:\n')
-	for proc in proc_list:
-		stdout.write("\t" + proc.to_str() + "\n")
+	
+	process_list = get_process_list()
+	
+	stdout.write('Reading Process list...\n')
+	
 		
+	process_cmd=[]
+	process_pid=[]
 
-	proc_cmd=[]
-	proc_pid=[]
+	for process in process_list:
+#		stdout.write("\t" + process.name()+ "\n")	
+    		process_cmd.append(process.name())
+		process_pid.append(process.procid())		
 
-	for proc in proc_list:
-		stdout.write("\t" + proc.name()+ "\n")	
-    		proc_cmd.append(proc.name())
-		proc_pid.append(proc.procid())		
-
-	print(proc_cmd)
+#	print(process_cmd)
 
 	l1 = ["logkey","keylog","keysniff","kisni","lkl","ttyrpld","uber","vlogger"]
 
 	record=0
 	flag=1
 
-    	for x in proc_cmd:
+    	for x in process_cmd:
 		for y in l1:
 			if(x.find(y)>-1):
-				print(" KeyLogger Detected: "+proc_pid[record]+" ---> "+x)
-				kill_logger(proc_pid[record])
+				stdout.write("KeyLogger Detected: \nThe following proccess may be a key logger: \n\n\t"+process_pid[record]+" ---> "+x)
+				kill_logger(process_pid[record])
 				flag=0
 		record+=1
 				
